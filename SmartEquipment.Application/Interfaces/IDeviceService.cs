e2a16d957e296fe68a5396c3aee51c65c678a3dc
ChiaFeng <ksm51112@gmail.com>
2026-04-07 23:05:11 +0800

    feat: define device service interface

---
+++ SmartEquipment.Application/Interfaces/IDeviceService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartEquipment.Domain.Entities;
+
+namespace SmartEquipment.Application.Interfaces
+{
+    public interface IDeviceService
+    {
+        Device StartDevice(int id);
+        Device StopDevice(int id);
+        Device GetStatus(int id);
+    }
+}
