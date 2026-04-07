320155aadee62950e7244429b3c27e789835122f
ChiaFeng <ksm51112@gmail.com>
2026-04-07 23:02:56 +0800

    feat: add device domain model

---
+++ SmartEquipment.Domain/Entities/Device.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartEquipment.Domain.Entities
+{
+    public class Device
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        // 狀態
+        public bool IsRunning { get; set; }
+
+        // 溫度
+        public double Temperature { get; set; }
+
+        // 最後更新時間
+        public DateTime LastUpdated { get; set; }
+    }
+}
