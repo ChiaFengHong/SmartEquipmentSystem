cd94fd2d91f6f0b09c75d3e9ec484833699e8536
ChiaFeng <ksm51112@gmail.com>
2026-04-07 23:04:50 +0800

    feat: implement device service

---
+++ SmartEquipment.Application/Services/DeviceService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartEquipment.Application.Interfaces;
+using SmartEquipment.Domain.Entities;
+
+namespace SmartEquipment.Application.Services
+{
+    public class DeviceService : IDeviceService
+    {
+        private static List<Device> _devices = new()
+    {
+        new Device { Id = 1, Name = "Machine-A", IsRunning = false, Temperature = 25 }
+    };
+
+        public Device StartDevice(int id)
+        {
+            var device = _devices.First(d => d.Id == id);
+
+            if (device.IsRunning)
+                throw new Exception("Device already running");
+
+            device.IsRunning = true;
+            device.LastUpdated = DateTime.Now;
+
+            return device;
+        }
+
+        public Device StopDevice(int id)
+        {
+            var device = _devices.First(d => d.Id == id);
+
+            if (!device.IsRunning)
+                throw new Exception("Device already stopped");
+
+            device.IsRunning = false;
+            device.LastUpdated = DateTime.Now;
+
+            return device;
+        }
+
+        public Device GetStatus(int id)
+        {
+            return _devices.First(d => d.Id == id);
+        }
+    }
+}
