65180f3cf365276a895d90b1acc5b8fb834d001f
ChiaFeng <ksm51112@gmail.com>
2026-04-07 23:05:52 +0800

    test: add device service test

---
+++ SmartEquipment.Tests/DeviceServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartEquipment.Application.Services;
+using Xunit;
+
+namespace SmartEquipment.Tests
+{
+    public class DeviceServiceTests
+    {
+        [Fact]
+        public void StartDevice_Should_Set_IsRunning_True()
+        {
+            var service = new DeviceService();
+
+            var result = service.StartDevice(1);
+
+            Assert.Throws<Exception>(() => service.StartDevice(1));
+        }
+    }
+}
